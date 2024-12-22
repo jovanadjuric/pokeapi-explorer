9f61038c6e3d043cd8052f869cd1df51464e3af1
Jovana Djuric <mat.jovana@gmail.com>
2024-12-22 23:09:37 +0100

    feat: add caching

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,16 +3,20 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/jovanadjuric/pokedex/internal/pokecache"
 )
 
 // Client -
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
 // NewClient -
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout time.Duration, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(5 * cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
